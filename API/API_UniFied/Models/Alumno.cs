97d01ed6034c0e3727f8758f43a3c50e9a94cbe2
Blancaaa7 <blancaperezc77@gmail.com>
2025-04-01 11:06:27 +0200

    hola
    clase añadida "alumnos"

---
+++ API/API_UniFied/Models/Alumno.cs
@@ -0,0 +1,50 @@
+﻿namespace API_UniFied.Models
+{
+    public enum Genero
+    {
+        HOMBRE,
+        MUJER
+    }
+
+    public enum tipo_id
+    {
+        DNI,
+        PASAPORTE,
+        NIE
+    }
+
+    public enum Eneatipo
+    {
+        REFORMADOR,
+        AYUDADOR,
+        TRIUNFADOR,
+        INDIVIDUALISTA,
+        INVESTIGADOR,
+        LEAL,
+        ENTUSIASTA,
+        DESAFIADOR,
+        PACIFICADOR,
+    }
+
+    public enum Estudios
+    {
+        GRADO, 
+        CETIS, 
+        MASTER
+    }
+
+    public enum Facultad
+    {
+        DERECHO_EMPRESA_GOBIERNO,
+
+    }
+
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public required string Nombre { get; set; }
+        public required string Apellido1 { get; set; }
+        public required string Apellido2 { get; set; }
+        public int Edad {  get; set; }
+    }
+}
